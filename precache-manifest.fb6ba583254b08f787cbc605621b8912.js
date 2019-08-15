45bb5349783510b421f30380d94416a1527dcf8a
Niyaz Nazipov <niyaztele@gmail.com>
2019-08-15 15:47:59 +0300

    Updates

---
+++ precache-manifest.fb6ba583254b08f787cbc605621b8912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41c9abd290a311346ee570e251f4aec",
+    "url": "/vkoauth/index.html"
+  },
+  {
+    "revision": "3d9ad7c13c69b6a8b7d6",
+    "url": "/vkoauth/static/css/main.48b03ce8.chunk.css"
+  },
+  {
+    "revision": "e9322a2019a47322810c",
+    "url": "/vkoauth/static/js/2.0d8a2bc3.chunk.js"
+  },
+  {
+    "revision": "3d9ad7c13c69b6a8b7d6",
+    "url": "/vkoauth/static/js/main.a0a32118.chunk.js"
+  },
+  {
+    "revision": "e1baae038883e04acef8",
+    "url": "/vkoauth/static/js/runtime~main.61a04880.js"
+  }
+]);
